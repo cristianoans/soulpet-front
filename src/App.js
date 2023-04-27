522be56b153cd6375bb3d75655ddd20ee87f0a45
Fernanda-Franco <fernandaf.vet@gmail.com>
2023-04-27 18:21:15 -0300

    Implementação da lista de Pets

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Home } from "./pages/Home/Home";
 import { NovoCliente } from "./pages/NovoCliente/NovoCliente";
 import { Clientes } from "./pages/Clientes/Clientes";
 import { EditaCliente } from "./pages/EditaCliente/EditaCliente";
+import { Pets } from "./pages/Pets/Pets";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Root />}>
           <Route path="/" element={<Home />} />
           <Route path="/clientes" element={<Clientes />} />
+          <Route path="/pets" element={<Pets />} />
           <Route path="/clientes/novo" element={<NovoCliente />} />
           <Route path="/clientes/editar/:id" element={<EditaCliente />} />
         </Route>
---
+++ src/pages/Pets/Pets.jsx
@@ -0,0 +1,69 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Button, Table } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { Loader } from "../../components/Loader/Loader";
+
+export function Pets() {
+    const [listaPets, setListaPets] = useState(null);
+
+    useEffect(() => {
+        initializeTable();
+    }, []);
+
+    function initializeTable() {
+        axios.get("http://localhost:3001/pets")
+            .then((response) => {
+                setListaPets(response.data);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }
+
+return (
+    <div className="pets container">
+        <div className="d-flex justify-content-between align-items-center">
+            <h1>Pets</h1>
+            <Button>
+                <i className="bi bi-plus-lg me-2"></i> Pet
+            </Button>
+        </div>
+        {listaPets === null ? (
+            <Loader />
+        ) : (
+            <Table striped bordered hover>
+                <thead>
+                    <tr>
+                        <th>Nome</th>
+                        <th>Tipo</th>
+                        <th>Porte</th>
+                        <th>DataNasc</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {listaPets.map((pet) => {
+                        return (
+                            <tr key={pet.id}>
+                                <td>{pet.nome}</td>
+                                <td>{pet.tipo}</td>
+                                <td>{pet.porte}</td>
+                                <td>{pet.dataNasc}</td>
+                                <td className="d-flex gap-2">
+                                    <Button>
+                                        <i className="bi bi-trash-fill"></i>
+                                    </Button>
+                                    <Button>
+                                        <i className="bi bi-pencil-fill"></i>
+                                    </Button>
+                                </td>
+                            </tr>
+                        );
+                    })}
+                </tbody>
+            </Table>
+        )}
+        ;
+    </div>
+);
+                }
