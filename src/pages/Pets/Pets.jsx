bcfd80db4c0995637f4cecca57f2f5bb1ce39d1a
unknown <cristianoans@hotmail.com>
2023-04-30 11:30:30 -0300

    inclusão do link para a página add pet, task FE-3.

--- src/pages/Pets/Pets.jsx
+++ src/pages/Pets/Pets.jsx
@@ -40,7 +40,7 @@ export function Pets() {
         <div className="pets container">
             <div className="d-flex justify-content-between align-items-center">
                 <h1>Pets</h1>
-                <Button>
+                <Button as={Link} to="/pets/novo">
                     <i className="bi bi-plus-lg me-2"></i> Pet
                 </Button>
             </div>
