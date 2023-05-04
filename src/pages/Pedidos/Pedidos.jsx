d00f2015dca440be32853bb27678d7c076d5764e
unknown <cristianoans@hotmail.com>
2023-05-04 09:09:12 -0300

    implementação do método de deleção do pedido, task FE-20.

--- src/pages/Pedidos/Pedidos.jsx
+++ src/pages/Pedidos/Pedidos.jsx
@@ -1,13 +1,39 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { Button, Table } from "react-bootstrap";
+import { Button, Modal, Table } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { Loader } from "../../components/Loader/Loader";
+import { toast } from "react-hot-toast";
 
 
 export function Pedidos() {
 
     const [pedidos, setPedidos] = useState(null);
+    const [show, setShow] = useState(false);
+    const [codigoPedido, setCodigoPedido] = useState(null);
+
+    const handleClose = () => {
+        setCodigoPedido(null);
+        setShow(false)
+    };
+
+    const handleShow = (codigo) => {
+        setCodigoPedido(codigo);
+        setShow(true)
+    };
+
+    function onDelete() {
+        axios.delete(`http://localhost:3001/pedidos/${codigoPedido}`)
+            .then(response => {
+                toast.success(response.data.message, { position: "bottom-right", duration: 2000 });
+                consultarPedidos();
+            })
+            .catch(error => {
+                console.log(error);
+                toast.error(error.response.data.message, { position: "bottom-right", duration: 2000 });
+            });
+        handleClose();
+    }
 
     useEffect(() => {
         consultarPedidos();
@@ -51,7 +77,7 @@ export function Pedidos() {
                                         <td>{pedido.produto.nome}</td>
                                         <td>{pedido.quantidade}</td>
                                         <td className="d-flex justify-content-center gap-2">
-                                            <Button>
+                                            <Button onClick={() => handleShow(pedido.codigo)}>
                                                 <i className="bi bi-trash-fill"></i>
                                             </Button>
                                             <Button>
@@ -64,6 +90,20 @@ export function Pedidos() {
                         </tbody>
                     </Table>
             }
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Confirmação</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>Tem certeza que deseja excluir o pedido?</Modal.Body>
+                <Modal.Footer>
+                    <Button variant="danger" onClick={handleClose}>
+                        Cancelar
+                    </Button>
+                    <Button variant="primary" onClick={onDelete}>
+                        Excluir
+                    </Button>
+                </Modal.Footer>
+            </Modal>
         </div>
     );
 }
